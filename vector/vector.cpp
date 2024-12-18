3788089ef45c17da2291add8bfb8922aa22230f9
Cheng Lei <attic.mascot@msa.hinet.net>
2024-12-18 23:32:20 +0800

    vector

---
+++ vector/vector.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> ary;
+    ary.push_back(304);
+    ary.push_back(262);
+    ary.push_back(208);
+    for (int i = 0; i < ary.size(); ++i)
+    {
+        std::cout << ary[i] << ", ";
+    }
+}
+
